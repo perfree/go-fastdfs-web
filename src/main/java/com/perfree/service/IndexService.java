3ef2e1e7bc1e65489e7b3238bc94ace86b419824
perfree <perfree@126.com>
2022-02-10 14:04:16 +0800

    更新依赖版本, 修复首页偶发性报错问题

--- src/main/java/com/perfree/service/IndexService.java
+++ src/main/java/com/perfree/service/IndexService.java
@@ -36,34 +36,34 @@ public class IndexService {
         JSONObject parseObj = JSONUtil.parseObj(data);
         JSONArray parseArray = JSONUtil.parseArray(parseObj.get("Fs.FileStats"));
         // 剩余空间
-        result.put("diskFreeSize", FileSizeUtil.GetLength(Long.parseLong(JSONUtil.parseObj(parseObj.get("Sys.DiskInfo")).getStr("free"))));
+        result.put("diskFreeSize", FileSizeUtil.GetLength(Long.parseLong(JSONUtil.parseObj(parseObj.get("Sys.DiskInfo")).getStr("free" ,"0"))));
         // 总空间
-        result.put("diskTotalSize", FileSizeUtil.GetLength(Long.parseLong(JSONUtil.parseObj(parseObj.get("Sys.DiskInfo")).getStr("total"))));
+        result.put("diskTotalSize", FileSizeUtil.GetLength(Long.parseLong(JSONUtil.parseObj(parseObj.get("Sys.DiskInfo")).getStr("total","0"))));
         // 已使用空间
-        result.put("diskUsedSize", FileSizeUtil.GetLength(Long.parseLong(JSONUtil.parseObj(parseObj.get("Sys.DiskInfo")).getStr("used"))));
+        result.put("diskUsedSize", FileSizeUtil.GetLength(Long.parseLong(JSONUtil.parseObj(parseObj.get("Sys.DiskInfo")).getStr("used","0"))));
         // 节点
-        result.put("inodesTotal", JSONUtil.parseObj(parseObj.get("Sys.DiskInfo")).getStr("inodesTotal"));
+        result.put("inodesTotal", JSONUtil.parseObj(parseObj.get("Sys.DiskInfo")).getStr("inodesTotal","0"));
         // 节点
-        result.put("inodesUsed", JSONUtil.parseObj(parseObj.get("Sys.DiskInfo")).getStr("inodesUsed"));
+        result.put("inodesUsed", JSONUtil.parseObj(parseObj.get("Sys.DiskInfo")).getStr("inodesUsed","0"));
         // 节点
-        result.put("inodesFree", JSONUtil.parseObj(parseObj.get("Sys.DiskInfo")).getStr("inodesFree"));
+        result.put("inodesFree", JSONUtil.parseObj(parseObj.get("Sys.DiskInfo")).getStr("inodesFree","0"));
         long dayFileSize = 0;
         long dayFileCount = 0;
         for (int i = 0; i < parseArray.size(); i++) {
             JSONObject fileStats = JSONUtil.parseObj(parseArray.getStr(i));
             if (fileStats.get("date").equals("all")) {
                 // 获取总文件大小,数量
-                result.put("totalFileSize", FileSizeUtil.GetLength(Long.parseLong(fileStats.getStr("totalSize"))));
-                result.put("totalFileCount", fileStats.getStr("fileCount"));
+                result.put("totalFileSize", FileSizeUtil.GetLength(Long.parseLong(fileStats.getStr("totalSize","0"))));
+                result.put("totalFileCount", fileStats.getStr("fileCount","0"));
             } else {
                 try {
                     long subDay = DateUtil.daysBetween(DateUtil.StrToDate(fileStats.getStr("date"), "yyyyMMdd"), new Date(), false);
                     if (subDay <= 30) {
                         // 获取30天内文件大小,数量
-                        dayFileSize += Long.parseLong(fileStats.getStr("totalSize"));
-                        dayFileCount += Long.parseLong(fileStats.getStr("fileCount"));
-                        dayFileSizeList.add(FileSizeUtil.GetMBLength(Long.parseLong(fileStats.getStr("totalSize"))));
-                        dayFileCountList.add(fileStats.getStr("fileCount"));
+                        dayFileSize += Long.parseLong(fileStats.getStr("totalSize","0"));
+                        dayFileCount += Long.parseLong(fileStats.getStr("fileCount","0"));
+                        dayFileSizeList.add(FileSizeUtil.GetMBLength(Long.parseLong(fileStats.getStr("totalSize","0"))));
+                        dayFileCountList.add(fileStats.getStr("fileCount","0"));
                         dayNumList.add(fileStats.getStr("date").substring(6) + "日");
                     }
                 } catch (ParseException e) {
